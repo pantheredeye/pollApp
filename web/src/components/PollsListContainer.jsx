eb208b64c082f95b09b6fa467fc98de0e01967e7
Barrett <barrettburnworth@gmail.com>
2022-08-08 17:00:21 -0500

    initial conversion

---
+++ web/src/components/PollsListContainer.jsx
@@ -0,0 +1,308 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+import update from 'react-addons-update'
+
+import AddingPollComponent from './AddingPollComponent'
+import EditPoll from './EditPoll'
+import ErrorComponent from './ErrorComponent'
+import PollForm from './PollForm'
+import PollsList from './PollsList'
+import VotePoll from './VotePoll'
+require('dotenv').config()
+
+const API_URL = process.env.REACT_APP_API_URL
+const API_HEADERS = {
+  'Content-Type': 'application/json',
+}
+
+class PollsListContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: undefined,
+      isLoaded: false,
+      routeState: RouteStates.PollList,
+    }
+
+    this.changeOption = (voteId) => {
+      let votesCount = this.state.votesCount
+      let unsetIndex = votesCount.findIndex((vote) => vote.selectedByUser)
+      let setIndex = votesCount.findIndex((vote) => vote.pollItem.id === voteId)
+      let query = {}
+      if (unsetIndex >= 0) {
+        query[unsetIndex] = {
+          selectedByUser: { $set: false },
+          total: { $apply: (v) => v - 1 },
+        }
+      }
+      if (setIndex >= 0) {
+        query[setIndex] = {
+          selectedByUser: { $set: true },
+          total: { $apply: (v) => v + 1 },
+        }
+      }
+      let newVotesCount = update(votesCount, query)
+      this.setState({ voteId: voteId, votesCount: newVotesCount })
+    }
+  }
+
+  componentDidMount() {
+    fetch(`${API_URL}/polls`, {
+      method: 'GET',
+      headers: API_HEADERS,
+    })
+      .then((res) =>
+        res.json().then(
+          (polls) => {
+            if (polls.error !== undefined) {
+              this.setState({
+                isLoaded: true,
+                error: polls.message,
+              })
+            } else {
+              this.setState({
+                isLoaded: true,
+                polls: polls,
+              })
+            }
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error: error.message,
+            })
+          }
+        )
+      )
+      .catch((error) => {
+        this.setState({
+          isLoaded: true,
+          error: error.message,
+        })
+      })
+  }
+
+  handleEditorCancel() {
+    this.setState({ routeState: RouteStates.PollList })
+  }
+
+  startAddPoll() {
+    this.setState({ routeState: RouteStates.AddPoll })
+  }
+
+  startEditPoll(pollId) {
+    let poll = this.getPoll(pollId)
+    if (poll !== undefined) {
+      this.setState({ routeState: RouteStates.EditPoll, poll: poll })
+    }
+  }
+
+  startVotePoll(pollId) {
+    let poll = this.getPoll(pollId)
+    if (poll !== undefined) {
+      fetch(`${API_URL}/votes?pollId=${pollId}`, { method: 'GET' })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Server response wasn't OK")
+          } else {
+            return response.json()
+          }
+        })
+        .then((votesCount) => {
+          let itemIndex = votesCount.findIndex((vote) => vote.selectedByUser)
+          let voteId
+          if (itemIndex >= 0) {
+            voteId = votesCount[itemIndex].pollItem.id
+          }
+          this.setState({
+            routeState: RouteStates.Vote,
+            poll: poll,
+            voteId: voteId,
+            votesCount: votesCount,
+          })
+        })
+        .catch((error) => {
+          this.setState({ error: error.message })
+        })
+    }
+  }
+
+  addPoll(poll) {
+    if (poll.id === null) {
+      poll = { ...poll, id: Date.now().toString() }
+    }
+    let newPolls = update(this.state.polls, { $push: [poll] })
+    let newState = {
+      error: undefined,
+      polls: newPolls,
+      routeState: RouteStates.PollList,
+    }
+    this.sendPoll(poll, 'POST', newState)
+  }
+
+  editPoll(poll) {
+    let pollIndex = this.state.polls.findIndex((poll_) => poll_.id === poll.id)
+    let newPolls = update(this.state.polls, { [pollIndex]: { $set: poll } })
+    let newState = {
+      error: undefined,
+      polls: newPolls,
+      routeState: RouteStates.PollList,
+    }
+    this.sendPoll(poll, 'PUT', newState)
+  }
+
+  sendPoll(poll, method, newState) {
+    let prevState = this.state
+    fetch(`${API_URL}/polls`, {
+      method: method,
+      body: JSON.stringify(poll),
+      headers: API_HEADERS,
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error("Server response wasn't OK")
+        }
+      })
+      .then((responseData) => {
+        poll.id = responseData.id
+        this.setState(newState)
+      })
+      .catch((error) => {
+        this.setState({
+          ...prevState,
+          error: error.message,
+          routeState: RouteStates.PollList,
+        })
+      })
+  }
+
+  votePoll(pollId, itemId) {
+    fetch(`${API_URL}/votes?pollId=${pollId}&option=${itemId}`, {
+      method: 'POST',
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Server response wasn't OK")
+        } else {
+          this.setState({ routeState: RouteStates.PollList })
+        }
+      })
+      .catch((error) => {
+        this.setState({ error: error.message })
+      })
+  }
+
+  deletePoll(pollId) {
+    let prevState = this.state
+    let pollIndex = this.state.polls.findIndex((poll) => poll.id === pollId)
+    let newPolls = update(this.state.polls, { $splice: [[pollIndex, 1]] })
+    fetch(`${API_URL}/polls?&pollId=${pollId}`, { method: 'DELETE' })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Server response wasn't OK")
+        } else {
+          this.setState({ polls: newPolls, routeState: RouteStates.PollList })
+        }
+      })
+      .catch((error) => {
+        this.setState({ ...prevState, error: error.message })
+      })
+  }
+
+  render() {
+    const { error, isLoaded } = this.state
+    let errorComponent
+    if (error) {
+      return <ErrorComponent message={'Ошибка: ' + error} />
+    } else if (!isLoaded) {
+      return <div>Загрузка...</div>
+    }
+    let childComponent = this.manualRoute(this.state.routeState)
+    return (
+      <div>
+        {errorComponent}
+        {childComponent}
+      </div>
+    )
+  }
+
+  manualRoute(routeState) {
+    switch (routeState) {
+      case RouteStates.PollList:
+        return (
+          <PollsList
+            user={this.props.user}
+            polls={this.state.polls}
+            addPoll={this.startAddPoll.bind(this)}
+            editPoll={this.startEditPoll.bind(this)}
+            votePoll={this.startVotePoll.bind(this)}
+            deletePoll={this.deletePoll.bind(this)}
+          />
+        )
+        break
+      case RouteStates.AddPoll:
+        return (
+          <AddingPollComponent
+            user={this.state.user}
+            onSubmit={this.addPoll.bind(this)}
+            onCancel={this.handleEditorCancel.bind(this)}
+          />
+        )
+        break
+      case RouteStates.EditPoll:
+        return (
+          <EditPoll
+            user={this.state.user}
+            poll={this.state.poll}
+            onSubmit={this.editPoll.bind(this)}
+            onCancel={this.handleEditorCancel.bind(this)}
+          />
+        )
+        break
+      case RouteStates.Vote:
+        return (
+          <VotePoll
+            user={this.state.user}
+            poll={this.state.poll}
+            voteId={this.state.voteId}
+            votesCount={this.state.votesCount}
+            onChangeOption={this.changeOption.bind(this)}
+            onVote={this.votePoll.bind(this)}
+            onCancel={this.handleEditorCancel.bind(this)}
+          />
+        )
+        break
+      default:
+        break
+    }
+    // By default use PollList
+    return <PollsListContainer user={this.state.user} />
+  }
+
+  getPoll(pollId) {
+    let pollIndex = this.state.polls.findIndex((poll) => poll.id === pollId)
+    if (pollIndex >= 0) {
+      return this.state.polls[pollIndex]
+    }
+    return undefined
+  }
+}
+
+PollForm.propTypes = {
+  user: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+}
+
+const RouteStates = {
+  PollList: 1,
+  AddPoll: 2,
+  EditPoll: 3,
+  Vote: 4,
+}
+
+export default PollsListContainer
---
+++ web/src/components/VotePoll.jsx
@@ -0,0 +1,102 @@
+import React from 'react'
+
+import PropTypes from 'prop-types'
+
+import PollForm from './PollForm'
+
+class VotePoll extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = (e) => {
+      //e.preventDefault();
+      this.props.onChangeOption(e.target.value)
+    }
+
+    this.handleSubmit = (e) => {
+      e.preventDefault()
+      this.props.onVote(this.props.poll.id, this.props.voteId)
+    }
+
+    this.handleClose = (e) => {
+      e.preventDefault()
+      this.props.onCancel()
+    }
+  }
+
+  render() {
+    let { poll, votesCount, voteId } = this.props
+    let totalVotes = votesCount.reduce((acc, vote) => acc + vote.total, 0)
+    if (totalVotes === 0) {
+      totalVotes = 1
+    }
+    let options = votesCount.map((vote) => {
+      let item = vote.pollItem
+      let percent = Math.round((vote.total / totalVotes) * 100).toString() + '%'
+      return (
+        <div key={item.id}>
+          <input
+            className="m-1"
+            type="radio"
+            name={item.id}
+            value={item.id}
+            checked={voteId === item.id}
+            onChange={this.handleChange}
+          />
+          {item.title}
+          <div className="progress">
+            <div
+              className="progress-bar"
+              role="progressbar"
+              style={{ width: percent }}
+            >
+              {vote.total}
+            </div>
+          </div>
+        </div>
+      )
+    })
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>
+          Select option you want to vote for &quot;{poll.title}&quot; poll:
+        </h4>
+        <div className="m-1">{options}</div>
+        <button className="btn btn-primary m-1" type="submit">
+          Save and close
+        </button>
+        <button className="btn btn-primary m-1" onClick={this.handleClose}>
+          Cancel
+        </button>
+      </form>
+    )
+  }
+}
+
+PollForm.propTypes = {
+  poll: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+      }).isRequired
+    ).isRequired,
+  }).isRequired,
+  voteId: PropTypes.string.isRequired,
+  votesCount: PropTypes.arrayOf(
+    PropTypes.shape({
+      pollItem: PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+      }).isRequired,
+      total: PropTypes.number.isRequired,
+      selectedByUser: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+  onChangeOption: PropTypes.func.isRequired,
+  onVote: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
+}
+
+export default VotePoll
